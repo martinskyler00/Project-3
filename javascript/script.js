8ad83f6214c2da877b2c2f08ce5fa444ffcb2134
martinskyler00 <112213373+martinskyler00@users.noreply.github.com>
2022-12-02 12:45:08 -0600

    fixed javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,23 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15.5,
-        center: { lat: 41.8730, lng: -87.6279},
-      });
-      var marker = new google.maps.Marker({
-        position: { lat: 41.90324, lng: -87.66789},
-        map: map,
-      });
-      var coffeeShop = new google.maps.Circle({
-        center:{ lat: 41.90324, lng: -87.66789},
-        map: map,
-        label: "Intelligentsia Coffee Wicker Park",
-        animation: google.maps.Animation.DROP
-      });
+function initMap(){
+  const coffeeShop = { lat: 41.90324, lng: -87.66789};
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: coffeeShop,
+  });
+
+
+  const marker = new google.maps.Marker({
+    position: coffeeShop,
+    map: map,
+    label: "Intelligentsia Coffee Wicker Park \n \n",
+    animation: google.maps.Animation.DROP,
+  });
+
+  const circle = new google.maps.Circle({
+    position: coffeeShop,
+    map: map,
+  });
+  
 }
 window.initMap = initMap;
